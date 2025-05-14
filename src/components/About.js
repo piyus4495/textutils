ede53dcf55e1c9980689525970be01c7e02bb699
piyus4495 <piyus4495@gmail.com>
2025-05-14 19:21:35 +0530

    Initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
     })
     const [btntext, setBtnText] = useState("Enable dark mode");
     const toggleStyle = ()=>{
-        if(myStyle.color == 'black'){
+        if(myStyle.color === 'black'){
             setMyStyle({
                 color: 'white',
                 backgroundColor: 'black'
